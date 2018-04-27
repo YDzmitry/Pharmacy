15d52a5571660b8a08467e0e454f22f5aa5cd517
Dzmitry Yanusik <yanusikdima@yandex.ru>
2018-04-27 15:47:17 +0300

    Added gitignore and BaseEntity

---
+++ src/main/java/com/vironit/pharmacy/model/BaseEntity.java
@@ -0,0 +1,19 @@
+package com.vironit.pharmacy.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@MappedSuperclass
+public class BaseEntity implements Serializable{
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+
+}
