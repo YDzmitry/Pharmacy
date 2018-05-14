2b46c1344e1046d50079d0b54c824683757d2304
Dzmitry Yanusik <yanusikdima@yandex.ru>
2018-05-14 15:07:25 +0300

    hot fix

--- src/main/java/com/vironit/pharmacy/model/BaseEntity.java
+++ src/main/java/com/vironit/pharmacy/model/BaseEntity.java
@@ -8,7 +8,7 @@ public class BaseEntity implements Serializable {
 
     @Id
     @Column(name = "id")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long id;
 
     public Long getId() {
