7e213dc421364bff87611b7e0a8f6d2216a5d082
Dzmitry Yanusik <yanusikdima@yandex.ru>
2018-06-05 23:24:28 +0300

    Added Cors

--- src/main/java/com/vironit/pharmacy/config/WebSecurityConfig.java
+++ src/main/java/com/vironit/pharmacy/config/WebSecurityConfig.java
@@ -4,12 +4,26 @@ import com.vironit.pharmacy.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Arrays;
+
+@CrossOrigin(origins = "http://localhost:8080")
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,33 +32,59 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private LoginService loginService;
 
     @Autowired
+    RoleBasedAuthenticationSuccessHandler roleBasedAuthenticationSuccessHandler;
+
+    @Autowired
     public void registerGlobalAuthentication(AuthenticationManagerBuilder auth) throws Exception {
         auth
                 .userDetailsService(loginService)
                 .passwordEncoder(passwordEncoder());
+
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        http.cors();
         http.csrf().disable();
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasAuthority("ADMIN")
                 .antMatchers("/customer/**").hasAuthority("CUSTOMER")
-                .antMatchers("/manager/**").hasAuthority("MANAGER");
+                .antMatchers("/manager/**").hasAuthority("MANAGER")
+                .antMatchers(HttpMethod.GET,"/order/get/**").hasAnyAuthority("ADMIN","CUSTOMER")
+                .antMatchers(HttpMethod.DELETE,"/order/delete/**").hasAuthority("ADMIN");
         http.formLogin()
                 .loginPage("/login")
                 .loginProcessingUrl("/loginUser")
-                .usernameParameter("login").passwordParameter("password")
-                .defaultSuccessUrl("/secure/home")
+                .usernameParameter("login")
+                .passwordParameter("password")
+                .successHandler(roleBasedAuthenticationSuccessHandler)
                 .failureForwardUrl("/err");
         http.logout().logoutUrl("/logout")
                 .logoutSuccessUrl("/index")
                 .invalidateHttpSession(true);
     }
 
+
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public RedirectStrategy redirectStrategy(){
+        return new DefaultRedirectStrategy();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
     }
 
 }
